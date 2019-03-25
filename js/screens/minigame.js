c69ad033027709d17005e03b3d4443204b4c9511
Seth Puckett <puckett.seth@gmail.com>
2019-03-25 16:53:08 -0400

    add submit answer logic

--- js/screens/minigame.js
+++ js/screens/minigame.js
@@ -5,14 +5,17 @@ minigame.init = function() { }
 minigame.preload = function() { }
 
 minigame.create = function() {
-  this.textEntry = this.add.text(10, 50, '', { font: '32px Courier', fill: '#ffff00' });
+  this.textEntry = this.add.text(10, this.cameras.main.height - 50, '', { font: '32px Courier', fill: '#ffff00' });
   this.keys = this.input.keyboard.addKeys('SPACE, BACKSPACE, ENTER, A,B,C')
 
   this.input.keyboard.on('keydown', function (event) {
     if (event.keyCode === this.keys.BACKSPACE.keyCode  && this.textEntry.text.length > 0) {
       this.textEntry.text = this.textEntry.text.substr(0, this.textEntry.text.length - 1);
-    } else if (isLetter(event.keyCode)) {
+    } else if (isLetter(event.keyCode) || event.keyCode === this.keys.SPACE.keyCode) {
       this.textEntry.text += event.key;
+    } else if (event.keyCode === this.keys.ENTER.keyCode) {
+      submitAnswer();
+      this.textEntry.text = '';
     }
   }, this);
 }
@@ -21,4 +24,8 @@ minigame.update = function() { }
 
 function isLetter(keyCode) {
   return keyCode >= 65 && keyCode <= 90;
+}
+
+function submitAnswer() {
+  // TODO: compare this.textEntry.text against onscreen values
 }
