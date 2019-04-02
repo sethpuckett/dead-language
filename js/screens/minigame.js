6238f7b3d14f677286acc659edb249141299c272
Seth Puckett <puckett.seth@gmail.com>
2019-04-02 19:28:25 -0300

    single vocab word proof of concept

--- js/screens/minigame.js
+++ js/screens/minigame.js
@@ -2,7 +2,9 @@ let minigame = new Phaser.Scene('Minigame');
 
 minigame.init = function() { }
 
-minigame.preload = function() { }
+minigame.preload = function() {
+  this.load.image('zombie', 'assets/images/zombie.png');
+}
 
 minigame.create = function() {
   this.textEntry = this.add.text(10, this.cameras.main.height - 50, '', { font: '32px Courier', fill: '#ffff00' });
@@ -14,18 +16,30 @@ minigame.create = function() {
     } else if (isLetter(event.keyCode) || event.keyCode === this.keys.SPACE.keyCode) {
       this.textEntry.text += event.key;
     } else if (event.keyCode === this.keys.ENTER.keyCode) {
-      submitAnswer();
-      this.textEntry.text = '';
+      this.submitAnswer();
     }
   }, this);
+
+  this.zombie = this.add.sprite(100, 0, 'zombie')
+  this.zombie.setScale(.6, .6)
+  this.zombieSpeed = .2
+
+  this.zombieText = this.add.text(90, 30, vocab.words[0].language1, { font: '16px Courier' })
 }
 
-minigame.update = function() { }
+minigame.update = function() {
+  this.zombie.y += this.zombieSpeed
+  this.zombieText.y += this.zombieSpeed
+}
 
 function isLetter(keyCode) {
   return keyCode >= 65 && keyCode <= 90;
 }
 
-function submitAnswer() {
-  // TODO: compare this.textEntry.text against onscreen values
+minigame.submitAnswer = function() {
+  if (this.textEntry.text === vocab.words[0].language2) {
+    this.zombie.destroy()
+    this.zombieText.destroy()
+  }
+  this.textEntry.text = ''
 }
---
+++ js/vocab.js
@@ -0,0 +1,14 @@
+let vocab = {
+  words: [
+    { language1: 'time', language2: 'vez', gender: 'f', permitted: [], example: '' },
+    { language1: 'year', language2: 'año', gender: 'm', permitted: ['ano'], example: '' },
+    { language1: 'time', language2: 'tiempo', gender: 'm', permitted: [], example: '' },
+    { language1: 'day', language2: 'día', gender: 'f', permitted: ['dia'], example: '' },
+    { language1: 'thing', language2: 'cosa', gender: 'f', permitted: [], example: '' },
+    { language1: 'man', language2: 'hombre', gender: 'm', permitted: [], example: '' },
+    { language1: 'part', language2: 'parte', gender: 'f', permitted: [], example: '' },
+    { language1: 'life', language2: 'vida', gender: 'f', permitted: [], example: '' },
+    { language1: 'moment', language2: 'momento', gender: 'm', permitted: [], example: '' },
+    { language1: 'form', language2: 'forma', gender: 'f', permitted: [], example: '' },
+  ]
+}
