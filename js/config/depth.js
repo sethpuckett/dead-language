2a4599022b1f55030215a53d9bcc7994189ae1a8
Seth Puckett <puckett.seth@gmail.com>
2019-04-26 09:16:13 -0300

    add depth config

---
+++ js/config/depth.js
@@ -0,0 +1,14 @@
+export default {
+  minigame: {
+    background: 0,
+    splatter: 1,
+    wall: 2,
+    hud: 3,
+    entryText: 4,
+    // 1000 - 3999 reserved for zombies & zombie text
+    zombieTextBackground: 1000,
+    zombieText: 1001,
+    zombie: 2000,
+    shotBlast: 4000,
+  },
+};
