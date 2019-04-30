f49658dcaf7cbf1e6aaf0b8409674faf5b486802
Seth Puckett <puckett.seth@gmail.com>
2019-04-30 16:35:46 -0300

    add hud manager

--- js/screens/minigame/Minigame.js
+++ js/screens/minigame/Minigame.js
@@ -1,11 +1,12 @@
 import Phaser from 'phaser';
 import vocab from '../../vocab';
-import { depth, minigame, images, screens, fonts } from '../../config';
+import { depth, minigame, images, screens } from '../../config';
 import minigameUiHelper from '../ui/minigameUiHelper';
 import VocabWordManager from '../../languageContent/VocabWordManager';
 import MinigameZombieManager from './MinigameZombieManager';
 import MinigameSpawnManager from './MinigameSpawnManager';
 import MinigameStatusManager from './MinigameStatusManager';
+import MinigameHudManager from './MinigameHudManager';
 import keyboardHelper from '../../util/keyboardHelper';
 
 // let firestore = firebase.firestore()
@@ -26,12 +27,13 @@ export default class extends Phaser.Scene {
     this.zombieManager = new MinigameZombieManager(this, this.vocab);
     this.spawnManager = new MinigameSpawnManager(this, minigame.waves, this.vocab);
     this.statusManager = new MinigameStatusManager(this);
+    this.hudManager = new MinigameHudManager(this);
     this.score = 0;
     this.health = minigame.startHealth;
   }
 
   create() {
-    this.createUi();
+    this.hudManager.createHud();
     this.createBackground();
     this.createCollisions();
     this.createInput();
@@ -55,101 +57,13 @@ export default class extends Phaser.Scene {
     }
   }
 
-  createUi() {
-    const ui = minigameUiHelper(this.sys.game.config);
-    this.hudHeight = ui.hudHeight;
-    this.hudBufferHeight = ui.hudBufferHeight;
-
-    // HUD
-    this.weaponBorder = this.add.sprite(ui.weaponBorderX, ui.weaponBorderY, images.hudItemBorder);
-    this.weaponBorder.displayWidth = ui.weaponBorderWidth;
-    this.weaponBorder.displayHeight = ui.weaponBorderWidth;
-    this.weaponBorder.setOrigin(ui.weaponBorderOriginX, ui.weaponBorderOriginY);
-    this.weaponBorder.setDepth(depth.minigame.hud);
-    this.itemBorder = this.add.sprite(ui.itemBorderX, ui.itemBorderY, images.hudItemBorder);
-    this.itemBorder.displayWidth = ui.itemBorderWidth;
-    this.itemBorder.displayHeight = ui.itemBorderWidth;
-    this.itemBorder.setOrigin(ui.itemBorderOriginX, ui.itemBorderOriginY);
-    this.itemBorder.setDepth(depth.minigame.hud);
-    this.healthIcon = this.add.sprite(ui.healthIconX, ui.healthIconY, images.heart);
-    this.healthIcon.displayWidth = ui.healthIconWidth;
-    this.healthIcon.displayHeight = ui.healthIconWidth;
-    this.healthIcon.setOrigin(ui.healthIconOriginX, ui.healthIconOriginY);
-    this.healthIcon.setDepth(depth.minigame.hud);
-    this.healthBars = [];
-    for (let i = 0; i < minigame.maxHealth; i += 1) {
-      const prev = i !== 0 ? this.healthBars[i - 1] : this.healthIcon;
-      const bar = this.add.sprite(ui.healthValueX(prev), ui.healthValueY, images.health, 0);
-      bar.displayWidth = ui.healthValueWidth;
-      bar.displayHeight = ui.healthValueHeight;
-      bar.setOrigin(ui.healthValueOriginX, ui.healthValueOriginY);
-      bar.setDepth(depth.minigame.hud);
-      this.healthBars.push(bar);
-    }
-
-    this.healthIcon.setOrigin(ui.healthIconOriginX, ui.healthIconOriginY);
-    this.healthIcon.setDepth(depth.minigame.hud);
-    this.killIcon = this.add.sprite(ui.killIconX, ui.killIconY, images.skull);
-    this.killIcon.displayWidth = ui.killIconWidth;
-    this.killIcon.displayHeight = ui.killIconWidth;
-    this.killIcon.setOrigin(ui.killIconOriginX, ui.killIconOriginY);
-    this.killIcon.setDepth(depth.minigame.hud);
-    this.killValue = this.add.bitmapText(
-      ui.killValueX, ui.killValueY, fonts.blueSkyWhite, '000', minigame.fonts.killSize
-    );
-    this.killValue.setOrigin(ui.killValueOriginX, ui.killValueOriginY);
-    this.killValue.setDepth(depth.minigame.hud);
-    this.cashIcon = this.add.sprite(ui.cashIconX, ui.cashIconY, images.goldCoin);
-    this.cashIcon.displayWidth = ui.cashIconWidth;
-    this.cashIcon.displayHeight = ui.cashIconWidth;
-    this.cashIcon.setOrigin(ui.cashIconOriginX, ui.cashIconOriginY);
-    this.cashIcon.setDepth(depth.minigame.hud);
-    this.cashValue = this.add.bitmapText(
-      ui.cashValueX, ui.cashValueY, fonts.blueSkyWhite, '$100', minigame.fonts.cashSize
-    );
-    this.cashValue.setOrigin(ui.cashValueOriginX, ui.cashValueOriginY);
-    this.cashValue.setDepth(depth.minigame.hud);
-    this.timerIcon = this.add.sprite(ui.timerIconX, ui.timerIconY, images.watch);
-    this.timerIcon.displayWidth = ui.timerIconWidth;
-    this.timerIcon.displayHeight = ui.timerIconWidth;
-    this.timerIcon.setOrigin(ui.timerIconOriginX, ui.timerIconOriginY);
-    this.timerIcon.setDepth(depth.minigame.hud);
-    this.timerValue = this.add.bitmapText(
-      ui.timerValueX, ui.timerValueY, fonts.blueSkyWhite, '', minigame.fonts.timerSize
-    );
-    this.timerValue.setOrigin(ui.timerValueOriginX, ui.timerValueOriginY);
-    this.timerValue.setDepth(depth.minigame.hud);
-    this.messageBorder = this.add.sprite(
-      ui.messageBorderX,
-      ui.messageBorderY,
-      images.hudMessageBorder
-    );
-    this.messageBorder.displayWidth = ui.messageBorderWidth;
-    this.messageBorder.displayHeight = ui.messageBorderHeight;
-    this.messageBorder.setOrigin(ui.messageBorderOriginX, ui.messageBorderOriginY);
-    this.messageBorder.setDepth(depth.minigame.hud);
-
-    // Text Entry
-    this.textEntryGraphics = this.add.graphics({ fillStyle: minigame.ui.textEntryStyle });
-    this.textEntryArea = new Phaser.Geom.Rectangle(
-      ui.textEntryAreaX, ui.textEntryAreaY, ui.textEntryAreaWidth, ui.textEntryAreaHeight
-    );
-    this.textEntryGraphics.fillRectShape(this.textEntryArea);
-    this.textEntryGraphics.setDepth(depth.minigame.hud);
-    this.textEntry = this.add.bitmapText(
-      ui.textEntryX, ui.textEntryY, fonts.blueSkyWhite, '', minigame.fonts.textEntrySize
-    );
-    this.textEntry.setOrigin(ui.textEntryOriginX, ui.textEntryOriginY);
-    this.textEntry.setDepth(depth.minigame.entryText);
-  }
-
   createBackground() {
     const ui = minigameUiHelper(this.sys.game.config);
 
     this.background = this.add.tileSprite(
       0, 0,
       this.sys.game.config.width,
-      this.sys.game.config.height - this.hudHeight,
+      this.sys.game.config.height - this.hudManager.getHudHeight(),
       images.grass,
     );
     this.background.setOrigin(0, 0);
@@ -168,9 +82,11 @@ export default class extends Phaser.Scene {
     this.zombieManager.setHitArea(
       new Phaser.Geom.Rectangle(
         0,
-        this.sys.game.config.height - this.hudHeight - this.hudBufferHeight,
+        this.sys.game.config.height
+          - this.hudManager.getHudHeight()
+          - this.hudManager.getHudBufferHeight(),
         this.sys.game.config.width,
-        this.hudHeight
+        this.hudManager.getHudHeight()
       )
     );
   }
---
+++ js/screens/minigame/MinigameHudManager.js
@@ -0,0 +1,110 @@
+import Phaser from 'phaser';
+import minigameUiHelper from '../ui/minigameUiHelper';
+import { depth, minigame, images, fonts } from '../../config';
+
+export default class {
+  constructor(scene) {
+    this.scene = scene;
+  }
+
+  createHud() {
+    const ui = minigameUiHelper(this.scene.sys.game.config);
+    this.hudHeight = ui.hudHeight;
+    this.hudBufferHeight = ui.hudBufferHeight;
+
+    // HUD
+    this.scene.weaponBorder = this.scene.add.sprite(
+      ui.weaponBorderX, ui.weaponBorderY, images.hudItemBorder
+    );
+    this.scene.weaponBorder.displayWidth = ui.weaponBorderWidth;
+    this.scene.weaponBorder.displayHeight = ui.weaponBorderWidth;
+    this.scene.weaponBorder.setOrigin(ui.weaponBorderOriginX, ui.weaponBorderOriginY);
+    this.scene.weaponBorder.setDepth(depth.minigame.hud);
+    this.scene.itemBorder = this.scene.add.sprite(
+      ui.itemBorderX, ui.itemBorderY, images.hudItemBorder
+    );
+    this.scene.itemBorder.displayWidth = ui.itemBorderWidth;
+    this.scene.itemBorder.displayHeight = ui.itemBorderWidth;
+    this.scene.itemBorder.setOrigin(ui.itemBorderOriginX, ui.itemBorderOriginY);
+    this.scene.itemBorder.setDepth(depth.minigame.hud);
+    this.scene.healthIcon = this.scene.add.sprite(ui.healthIconX, ui.healthIconY, images.heart);
+    this.scene.healthIcon.displayWidth = ui.healthIconWidth;
+    this.scene.healthIcon.displayHeight = ui.healthIconWidth;
+    this.scene.healthIcon.setOrigin(ui.healthIconOriginX, ui.healthIconOriginY);
+    this.scene.healthIcon.setDepth(depth.minigame.hud);
+    this.scene.healthBars = [];
+    for (let i = 0; i < minigame.maxHealth; i += 1) {
+      const prev = i !== 0 ? this.scene.healthBars[i - 1] : this.scene.healthIcon;
+      const bar = this.scene.add.sprite(ui.healthValueX(prev), ui.healthValueY, images.health, 0);
+      bar.displayWidth = ui.healthValueWidth;
+      bar.displayHeight = ui.healthValueHeight;
+      bar.setOrigin(ui.healthValueOriginX, ui.healthValueOriginY);
+      bar.setDepth(depth.minigame.hud);
+      this.scene.healthBars.push(bar);
+    }
+
+    this.scene.healthIcon.setOrigin(ui.healthIconOriginX, ui.healthIconOriginY);
+    this.scene.healthIcon.setDepth(depth.minigame.hud);
+    this.scene.killIcon = this.scene.add.sprite(ui.killIconX, ui.killIconY, images.skull);
+    this.scene.killIcon.displayWidth = ui.killIconWidth;
+    this.scene.killIcon.displayHeight = ui.killIconWidth;
+    this.scene.killIcon.setOrigin(ui.killIconOriginX, ui.killIconOriginY);
+    this.scene.killIcon.setDepth(depth.minigame.hud);
+    this.scene.killValue = this.scene.add.bitmapText(
+      ui.killValueX, ui.killValueY, fonts.blueSkyWhite, '000', minigame.fonts.killSize
+    );
+    this.scene.killValue.setOrigin(ui.killValueOriginX, ui.killValueOriginY);
+    this.scene.killValue.setDepth(depth.minigame.hud);
+    this.scene.cashIcon = this.scene.add.sprite(ui.cashIconX, ui.cashIconY, images.goldCoin);
+    this.scene.cashIcon.displayWidth = ui.cashIconWidth;
+    this.scene.cashIcon.displayHeight = ui.cashIconWidth;
+    this.scene.cashIcon.setOrigin(ui.cashIconOriginX, ui.cashIconOriginY);
+    this.scene.cashIcon.setDepth(depth.minigame.hud);
+    this.scene.cashValue = this.scene.add.bitmapText(
+      ui.cashValueX, ui.cashValueY, fonts.blueSkyWhite, '$100', minigame.fonts.cashSize
+    );
+    this.scene.cashValue.setOrigin(ui.cashValueOriginX, ui.cashValueOriginY);
+    this.scene.cashValue.setDepth(depth.minigame.hud);
+    this.scene.timerIcon = this.scene.add.sprite(ui.timerIconX, ui.timerIconY, images.watch);
+    this.scene.timerIcon.displayWidth = ui.timerIconWidth;
+    this.scene.timerIcon.displayHeight = ui.timerIconWidth;
+    this.scene.timerIcon.setOrigin(ui.timerIconOriginX, ui.timerIconOriginY);
+    this.scene.timerIcon.setDepth(depth.minigame.hud);
+    this.scene.timerValue = this.scene.add.bitmapText(
+      ui.timerValueX, ui.timerValueY, fonts.blueSkyWhite, '', minigame.fonts.timerSize
+    );
+    this.scene.timerValue.setOrigin(ui.timerValueOriginX, ui.timerValueOriginY);
+    this.scene.timerValue.setDepth(depth.minigame.hud);
+    this.scene.messageBorder = this.scene.add.sprite(
+      ui.messageBorderX,
+      ui.messageBorderY,
+      images.hudMessageBorder
+    );
+    this.scene.messageBorder.displayWidth = ui.messageBorderWidth;
+    this.scene.messageBorder.displayHeight = ui.messageBorderHeight;
+    this.scene.messageBorder.setOrigin(ui.messageBorderOriginX, ui.messageBorderOriginY);
+    this.scene.messageBorder.setDepth(depth.minigame.hud);
+
+    this.scene.textEntryGraphics = this.scene.add.graphics({
+      fillStyle: minigame.ui.textEntryStyle,
+    });
+    this.scene.textEntryArea = new Phaser.Geom.Rectangle(
+      ui.textEntryAreaX, ui.textEntryAreaY, ui.textEntryAreaWidth, ui.textEntryAreaHeight
+    );
+    this.scene.textEntryGraphics.fillRectShape(this.scene.textEntryArea);
+    this.scene.textEntryGraphics.setDepth(depth.minigame.hud);
+    this.scene.textEntry = this.scene.add.bitmapText(
+      ui.textEntryX, ui.textEntryY, fonts.blueSkyWhite, '', minigame.fonts.textEntrySize
+    );
+    this.scene.textEntry.setOrigin(ui.textEntryOriginX, ui.textEntryOriginY);
+    this.scene.textEntry.setDepth(depth.minigame.entryText);
+  }
+
+  getHudHeight() {
+    return this.hudHeight;
+  }
+
+  getHudBufferHeight() {
+    return this.hudBufferHeight;
+  }
+}
