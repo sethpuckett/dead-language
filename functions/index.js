2dc57735ab00a631ee27029a10fb419a450c9fba
Seth Puckett <puckett.seth@gmail.com>
2019-05-22 20:03:03 +0200

    add comment

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
+// This method creates a user profile entry in firestore whenever a new user is created
 exports.addUserToDatabase = functions.auth.user().onCreate(user => {
   admin.firestore().collection('users').doc(user.uid).set({
     email: user.email,
