9215c2fa6a92ac3f79f4a0759f3ce91970400be3
Seth Puckett <puckett.seth@gmail.com>
2019-07-22 19:33:31 +0400

    add modal text config

---
+++ js/config/modalText.js
@@ -0,0 +1,30 @@
+import screens from './screens';
+
+export default [
+  {
+    id: 'minigame-intro',
+    stage: 'intro-01',
+    screen: screens.minigame,
+    text: [
+      [
+        'Those zombies will be here any second!',
+        'I sure hope ya got enough practice.',
+      ],
+      [
+        'Try to stay calm when ya see \'em.',
+        'Just focus on the vocab next to each one.',
+        'Type the translation and press \'Enter\'',
+        'to take \'em out.',
+      ],
+      [
+        'But don\'t dawdle! If a zombie gets through',
+        'they\'ll attack. Take too many hits and',
+        'yer toast. Ya just need to survive until',
+        'the timer reaches 0.',
+      ],
+      [
+        'Okay! Here they come! Look out!',
+      ],
+    ],
+  },
+];
