5e0c46b4dfadcde8c860854f2249904f637d6ad1
Seth Puckett <puckett.seth@gmail.com>
2019-08-06 12:46:57 +0400

    fix json formatting

--- js/config/lessonMap.js
+++ js/config/lessonMap.js
@@ -4,65 +4,46 @@ export default [
   {
     id: lessons.basicVocab.name,
     location: 'The Front Gate',
-    position: {
-      x: 0,
-      y: 0,
-    },
+    position: { x: 0, y: 0 },
   },
   {
     id: lessons.nouns1.name,
     location: 'Storage Sheds',
-    position: {
-      x: 1,
-      y: 0,
-    },
+    position: { x: 1, y: 0 },
   },
   {
     id: lessons.numbers1.name,
     location: 'Administration Building',
-    position: {
-      x: 1,
-      y: 1,
-    },
+    position: { x: 1, y: 1 },
   },
   // {
   //   id: lessons.numbers2.name,
   //   location: 'Accounting Office',
-  //   position: {
-  //     x: 0,
-  //     y: 1,
-  //   },
+  //   position: { : 0, y: 1 },
   // },
   {
     id: lessons.adjectives1.name,
     location: 'Econo-lux Apartments',
-    position: {
-      x: 2,
-      y: 1,
-    },
+    position: { x: 2, y: 1 },
   },
   {
     id: lessons.verbs1.name,
     location: 'Activity Center',
-    position: {
-      x: 2,
-      y: 2,
-    },
+    position: { x: 1, y: 2 },
   },
   {
     id: lessons.nouns2.name,
     location: 'Maintenance Building',
-    position: {
-      x: 2,
-      y: 0,
-    },
+    position: { x: 2, y: 0 },
   },
   {
     id: lessons.aroundTheHouse.name,
     location: 'Hillside Apartments',
-    position: {
-      x: 3,
-      y: 2,
-    },
+    position: { x: 3, y: 2 },
+  },
+  {
+    id: lessons.food1.name,
+    location: 'Rusty Spoon Restaurant',
+    position: { x: 2, y: 2 },
   },
 ];
