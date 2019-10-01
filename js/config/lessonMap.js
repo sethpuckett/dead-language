5ef35dcae81ec4374f2f706f65323f39ffcd74f9
Seth Puckett <puckett.seth@gmail.com>
2019-10-01 13:24:47 -0600

    update lesson map

--- js/config/lessonMap.js
+++ js/config/lessonMap.js
@@ -36,43 +36,25 @@ export default [
     location: 'Le Gros Cochon Brasserie',
     position: { x: 6, y: 1 },
   },
-
-
-
-
-
-
-
   {
-    id: lessons.nouns1.name,
-    location: 'Storage Sheds',
-    position: { x: 1, y: 0 },
-  },
-
-  {
-    id: lessons.adjectives1.name,
-    location: 'Econo-lux Apartments',
-    position: { x: 2, y: 1 },
+    id: lessons.aroundTheHouse1.name,
+    location: 'Bayview Apartments',
+    position: { x: 2, y: 3 },
   },
   {
-    id: lessons.verbs1.name,
-    location: 'Activity Center',
-    position: { x: 1, y: 2 },
+    id: lessons.aroundTheHouse2.name,
+    location: 'The Lofts at Dictionary Bay',
+    position: { x: 2, y: 4 },
   },
   {
-    id: lessons.nouns2.name,
-    location: 'Maintenance Building',
-    position: { x: 2, y: 0 },
+    id: lessons.school1.name,
+    location: 'High School',
+    position: { x: 1, y: 3 },
   },
   {
-    id: lessons.aroundTheHouse.name,
-    location: 'Hillside Apartments',
-    position: { x: 3, y: 2 },
-  },
-  {
-    id: lessons.school.name,
-    location: 'Hillside Elementary School',
-    position: { x: 3, y: 1 },
+    id: lessons.school2.name,
+    location: 'Dictionary Bay University',
+    position: { x: 0, y: 3 },
   },
   {
     id: lessons.office.name,
@@ -82,31 +64,166 @@ export default [
   {
     id: lessons.whenAndWhere.name,
     location: 'Antique Shop',
-    position: { x: 0, y: 2 },
+    position: { x: 1, y: 0 },
   },
   {
-    id: lessons.nature.name,
-    location: 'Hillside Park',
-    position: { x: 4, y: 2 },
+    id: lessons.nature1.name,
+    location: 'Public Park',
+    position: { x: 3, y: 4 },
   },
   {
-    id: lessons.bodyAndHealth.name,
-    location: 'Hillside Medical Center',
-    position: { x: 3, y: 3 },
+    id: lessons.nature2.name,
+    location: 'Nature Reserve',
+    position: { x: 4, y: 4 },
   },
   {
-    id: lessons.animals.name,
-    location: 'Hillside Pet Store',
+    id: lessons.bodyAndHealth1.name,
+    location: 'Medical Center',
+    position: { x: 5, y: 2 },
+  },
+  {
+    id: lessons.bodyAndHealth2.name,
+    location: 'Emergency Room',
+    position: { x: 7, y: 2 },
+  },
+  {
+    id: lessons.animals1.name,
+    location: 'Pet Store',
     position: { x: 4, y: 3 },
   },
   {
-    id: lessons.science.name,
-    location: 'Hillside Lab',
-    position: { x: 2, y: 4 },
+    id: lessons.animals2.name,
+    location: 'Petting Zoo',
+    position: { x: 5, y: 4 },
   },
   {
-    id: lessons.fashion.name,
-    location: 'Hillside Outlet Mall',
-    position: { x: 4, y: 1 },
+    id: lessons.science1.name,
+    location: 'Observatory',
+    position: { x: 1, y: 4 },
+  },
+  {
+    id: lessons.science1.name,
+    location: 'Research Facility',
+    position: { x: 0, y: 4 },
+  },
+  {
+    id: lessons.fashion1.name,
+    location: 'Outlet Mall',
+    position: { x: 5, y: 3 },
+  },
+  {
+    id: lessons.fashion2.name,
+    location: 'Dictionary Bay Boutique',
+    position: { x: 6, y: 4 },
+  },
+  {
+    id: lessons.sports.name,
+    location: 'Fitness Center',
+    position: { x: 3, y: 2 },
+  },
+  {
+    id: lessons.travel.name,
+    location: 'Bus Station',
+    position: { x: 0, y: 1 },
+  },
+  {
+    id: lessons.nouns1.name,
+    location: 'Storage Sheds',
+    position: { x: 1, y: 2 },
+  },
+  {
+    id: lessons.nouns2.name,
+    location: 'Student Housing',
+    position: { x: 3, y: 3 },
+  },
+  {
+    id: lessons.nouns3.name,
+    location: 'Condo Complex',
+    position: { x: 4, y: 2 },
+  },
+  {
+    id: lessons.nouns4.name,
+    location: 'Dollar Store',
+    position: { x: 6, y: 3 },
+  },
+  {
+    id: lessons.nouns5.name,
+    location: 'Department Store',
+    position: { x: 7, y: 3 },
+  },
+  {
+    id: lessons.nouns6.name,
+    location: 'Office Park',
+    position: { x: 7, y: 4 },
+  },
+  {
+    id: lessons.nouns7.name,
+    location: 'Assisted Living Complex',
+    position: { x: 8, y: 4 },
+  },
+  {
+    id: lessons.nouns8.name,
+    location: 'Flea Market',
+    position: { x: 8, y: 3 },
+  },
+  {
+    id: lessons.nouns9.name,
+    location: 'Junkyard',
+    position: { x: 8, y: 2 },
+  },
+  {
+    id: lessons.toolsAndHardware.name,
+    location: 'Hardware Store',
+    position: { x: 6, y: 2 },
+  },
+  {
+    id: lessons.adverbs1.name,
+    location: 'Library',
+    position: { x: 4, y: 0 },
+  },
+  {
+    id: lessons.adverbs2.name,
+    location: 'Courthouse',
+    position: { x: 7, y: 0 },
+  },
+  {
+    id: lessons.adjectives1.name,
+    location: 'Economy Apartments',
+    position: { x: 2, y: 2 },
+  },
+  {
+    id: lessons.adjectives2.name,
+    location: 'Luxury Apartments',
+    position: { x: 5, y: 0 },
+  },
+  {
+    id: lessons.adjectives3.name,
+    location: 'Police Station',
+    position: { x: 6, y: 0 },
+  },
+  {
+    id: lessons.verbs1.name,
+    location: 'Gym',
+    position: { x: 2, y: 1 },
+  },
+  {
+    id: lessons.verbs2.name,
+    location: 'Activity Center',
+    position: { x: 3, y: 0 },
+  },
+  {
+    id: lessons.verbs3.name,
+    location: 'Swimming Pool',
+    position: { x: 3, y: 0 },
+  },
+  {
+    id: lessons.verbs4.name,
+    location: 'Flight School',
+    position: { x: 3, y: 0 },
+  },
+  {
+    id: lessons.verbs5.name,
+    location: 'Boating Club',
+    position: { x: 3, y: 0 },
   },
 ];
